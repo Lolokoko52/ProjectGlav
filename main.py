bf739e45711a2e0d1d3d6cb8cdd4183448545215
Lolokoko52 <156549407+Lolokoko52@users.noreply.github.com>
2024-05-26 18:37:46 +0300

    Update main.py

--- main.py
+++ main.py
@@ -23,8 +23,16 @@ def weather(message):
 def get_weather(message):
     city = message.text.strip().lower()
     res = requests.get(f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API}&units=metric')
-    data = json.loads(res.text)
-    bot.reply_to(message, f'Сейчас погода: {res.json()}')
+    if res.status_code == 200:
+        data = json.loads(res.text)
+        temp = data["main"]["temp"]
+        bot.reply_to(message, f'Сейчас погода: {temp}')
+
+        image = "sunny.png" if temp > 5.0 else 'sun.png'
+        file = open('./' + image, 'rb')
+        bot.send_photo(message.chat.id, file)
+    else:
+        bot.send_message(message, 'Город указан не верно')
 def summa(message):
     global amount
     try:
